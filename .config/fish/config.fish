5bc292a800082d78117cb4f359c91e2b6c820fb2
Julian Andres Klode <jak@jak-linux.org>
2025-07-09 12:22:36 +0200

    fish: use zellij

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,4 @@ set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-if not set -q VSCODE_CWD;   status --is-login; and status --is-interactive; and exec byobu-launcher;end
+if not set -q ZELLIJ; and status --is-interactive; and exec zellij attach -c;end
